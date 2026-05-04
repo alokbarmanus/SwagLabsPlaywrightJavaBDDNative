792eb40be3992e6b8a2c996f3ec4189af96a38f2
AlokBarmanQA <alokbarmanqal@gmail.com>
2026-05-03 21:59:15 -0400

    Update-4: Refactor Hooks and ScenarioContext, remove debug wait, and update TestRunner config

--- src/test/java/com/qa/runners/TestRunner.java
+++ src/test/java/com/qa/runners/TestRunner.java
@@ -5,12 +5,12 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features", glue = { "com/qa/stepDefinitions",
-		"com.qa.hooks" }, plugin = { "pretty",
+@CucumberOptions(features = "src/test/resources/features", glue = { "com.qa.stepDefinitions",
+		"com.qa.hooks" }, plugin = {
+				"pretty",
 				"html:target/cucumber-reports.html",
 				"testng:target/testng-cucumber-reports.xml",
-				"testng:target/testng-cucumber-reports.html",
-				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, tags = "@regression")
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, tags = "@regression", monochrome = true)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
